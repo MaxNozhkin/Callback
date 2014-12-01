25ba780f483b4d6ca33eb4e9ff9c18d3ddce329b
Maksym Nozhkin <n.maxym@gmail.com>
2014-12-01 18:53:02 +0200

    create package

---
+++ src/Nmax/Callback/CallbackServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Nmax\Callback;
+
+use Illuminate\Support\ServiceProvider;
+
+class CallbackServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('nmax/callback');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
